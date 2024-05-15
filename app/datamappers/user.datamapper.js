1e6322186584783472567f9cb338e8bef0a1a801
solremi <jeremie.rocquet@outlook.fr>
2024-05-15 14:03:15 +0200

    fix: lunching app

--- app/datamappers/user.datamapper.js
+++ app/datamappers/user.datamapper.js
@@ -7,11 +7,14 @@ class UserDataMapper {
     async findUserByEmail(email) {
         const query = 'SELECT * FROM user WHERE email = $1';
         const result = await this.pool.query(query, [email]);
-        return result.rows.length > 0;
+        return result.rows.length > 0 || null;
     }
+
     async createUser(email, lastname, firstname, hashedPassword) {
         const query = 'INSERT INTO user (email, lastname, firstname, password) VALUES ($1, $2, $3, $4)';
         const values = [email, lastname, firstname, hashedPassword];
         await this.pool.query(query, values);
     }
 }
+
+export default UserDataMapper;
--- app/routers/main.router.js
+++ app/routers/main.router.js
@@ -3,6 +3,6 @@ import * as signupController from "../controllers/signup.controller.js";
 
 const router = express.Router();
 
-router.post("/signup",  signupController.createUSer);
+router.post("/signup",  signupController.createUser);
 
 export default router;
