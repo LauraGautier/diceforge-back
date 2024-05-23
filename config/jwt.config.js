f1c38995946adbb414107dbec56bec8ad8807653
Benjamin Beauvallet <benjamin.beauvallet@oclock.school>
2024-05-23 15:56:40 +0200

    Feat: addition of JWT to the connection and control of JWTs during requests

---
+++ config/jwt.config.js
@@ -0,0 +1,8 @@
+const jwtConfig = {
+    secretKey: '2dztl%-cv+@&dyjwz9848su!7wob316bn6op*ty9#&0gtuaz^', // Clé secrète pour signer les tokens JWT
+    options: {
+        expiresIn: '1h' // Token expirera après 1 heure
+    }
+};
+
+export default jwtConfig;
