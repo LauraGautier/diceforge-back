56d78a732e47951727c8ff88a3d03f9530c98530
solremi <jeremie.rocquet@outlook.fr>
2024-05-24 13:55:38 +0200

    feat: script deploy to deploiement

--- app/routers/main.router.js
+++ app/routers/main.router.js
@@ -5,15 +5,13 @@ import gameRouter from "./game.router.js";
 import sheetRouter from "./sheet.router.js";
 import profileRouter from "./profile.router.js";
 import isLoggedIn from "../middlewares/isLogin.middleware.js";
-import cw from "../middlewares/controllerWrapper.middleware.js";
-import jwtAuthMiddleware from "../middlewares/jwtAuth.middleware.js";
 
 const router = express.Router();
 
-router.use("/api", cw(authRouter));
-router.use("/api", cw(userRouter));
-router.use("/api", jwtAuthMiddleware, cw(isLoggedIn, gameRouter));
-router.use("/api", jwtAuthMiddleware, cw(isLoggedIn, sheetRouter));
-router.use("/api", jwtAuthMiddleware, cw(isLoggedIn, profileRouter));
+router.use("/api", authRouter);
+router.use("/api", userRouter);
+router.use("/api", isLoggedIn, gameRouter);
+router.use("/api", isLoggedIn, sheetRouter);
+router.use("/api", isLoggedIn, profileRouter);
 
 export default router;
