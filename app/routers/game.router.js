c09d312adb40e558427c82386f4bc0d29d0e0a75
solremi <jeremie.rocquet@outlook.fr>
2024-05-27 16:03:07 +0200

    feat: getgamesbyid router + controller + datamappers

--- app/routers/game.router.js
+++ app/routers/game.router.js
@@ -6,6 +6,7 @@ import jwtAuthMiddleware from "../middlewares/jwtAuth.middleware.js";
 const router = express.Router();
 
 router.get("/game", jwtAuthMiddleware, cw(gameController.getGame));
+router.get("/game/:id", jwtAuthMiddleware, cw(gameController.getGamesById));
 router.post("/game", jwtAuthMiddleware, cw(gameController.createGame));
 router.patch("/game/:id", jwtAuthMiddleware, cw(gameController.updateGame));
 router.delete("/game/:id", jwtAuthMiddleware, cw(gameController.deleteGame));
