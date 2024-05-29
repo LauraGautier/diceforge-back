b6b050b8e43a41000d2174f412e3adc0b2664340
LauraGautier <lc.gautier@icloud.com>
2024-05-29 10:04:19 +0200

    Updated websockets for chat

--- config/socket.config.js
+++ config/socket.config.js
@@ -6,6 +6,11 @@ function setupSocket(httpServer) {
     io.on('connection', socket => {
         console.log('Un client est connecté');
 
+        socket.on('message', (message) => {
+            console.log('Message reçu:', message);
+            socket.broadcast.emit('message', message);
+        });
+
         socket.on('disconnect', () => {
             console.log('Client déconnecté');
         });
