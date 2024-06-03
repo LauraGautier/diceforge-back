016a9f7eabef7ef74bedf315769d8cc75470ae0c
solremi <jeremie.rocquet@outlook.fr>
2024-06-03 09:38:57 +0200

    refacto jsdoc

--- app/controllers/refresh-token.controller.js
+++ app/controllers/refresh-token.controller.js
@@ -6,42 +6,6 @@ const { secretKey } = jwtConfig; // Extraction de la clé secrète de la configu
 
 // Fonction pour rafraîchir un token d'authentification
 export const refreshAccessToken = (req, res) => {
-    /**
-     * @swagger
-     * /refresh-token:
-     *   post:
-     *     summary: Refresh the access token
-     *     tags: [Authentification]
-     *     description: This endpoint refreshes the access token using the refresh token provided in the request body.
-     *     requestBody:
-     *       required: true
-     *       content:
-     *         application/json:
-     *           schema:
-     *             type: object
-     *             properties:
-     *               refreshToken:
-     *                 type: string
-     *     responses:
-     *       200:
-     *         description: Access token refreshed successfully
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 accessToken:
-     *                   type: string
-     *       403:
-     *         description: Invalid refresh token
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     */
     const refreshToken = req.body.refreshToken; // Extraction du token de rafraîchissement depuis le corps de la requête
 
     // Vérifier et décoder le token de rafraîchissement
--- app/controllers/sheet.controller.js
+++ app/controllers/sheet.controller.js
@@ -4,51 +4,6 @@ import SheetDataMapper from '../datamappers/sheet.datamapper.js';
 const sheetDataMapper = new SheetDataMapper(pool);
 
 export const getSheet = async (req, res) => {
-   /**
-     * @openapi
-     * /sheet:
-     *   get:
-     *     summary: Retrieve a sheet by name
-     *     tags: [Sheet]
-     *     description: This endpoint retrieves a sheet by its name.
-     *     parameters:
-     *       - in: query
-     *         name: name
-     *         required: true
-     *         schema:
-     *           type: string
-     *         description: The sheet name.
-     *     responses:
-     *       200:
-     *         description: Sheet retrieved successfully.
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 name:
-     *                   type: string
-     *                   description: The name of the sheet.
-     *                 // Example of additional properties:
-     *                 data:
-     *                   type: array
-     *                   description: Data contained in the sheet.
-     *                   items:
-     *                     type: object
-     *                     properties:
-     *                       // Define properties of data items
-     *       404:
-     *         description: Sheet not found.
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   description: Error message.
-     */
-
     const name = req.params.name;
     const sheet = await sheetDataMapper.findSheetByName(name);
 
@@ -59,31 +14,6 @@ export const getSheet = async (req, res) => {
 };
 
 export const createSheet = async (req, res) => {
-    /**
- * @openapi
- * /sheet:
- *   post:
- *     summary: Create a new sheet
- *     tags: [Sheet]
- *     description: This endpoint creates a new sheet.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               // Add the properties of the sheet object here
- *     responses:
- *       201:
- *         description: Sheet created successfully.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 // Add the properties of the created sheet object here
- */
     const sheet = req.body;
     const createdSheet = await sheetDataMapper.createSheet(sheet);
 
@@ -91,66 +21,6 @@ export const createSheet = async (req, res) => {
 }
 
 export const updateSheet = async (req, res) => {
-  /**
-     * @openapi
-     * /sheet/{name}:
-     *   patch:
-     *     summary: Update a sheet by name
-     *     tags: [Sheet]
-     *     description: This endpoint updates a sheet by its name.
-     *     parameters:
-     *       - in: path
-     *         name: name
-     *         required: true
-     *         schema:
-     *           type: string
-     *         description: The sheet name.
-     *     requestBody:
-     *       required: true
-     *       content:
-     *         application/json:
-     *           schema:
-     *             type: object
-     *             properties:
-     *               image:
-     *                 type: string
-     *                 description: URL or identifier of the image associated with the sheet.
-     *               class:
-     *                 type: string
-     *                 description: Classification or category of the sheet.
-     *               level:
-     *                 type: integer
-     *                 description: Level or priority of the sheet.
-     *     responses:
-     *       200:
-     *         description: Sheet updated successfully.
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 name:
-     *                   type: string
-     *                 data:
-     *                   type: array
-     *                   description: Data contained in the sheet.
-     *                   items:
-     *                     type: object
-     *                     properties:
-     *                       // Define specific properties of data items here
-     *       404:
-     *         description: Sheet not found.
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   description: Error message.
-     */
-
-
     const name = req.params.name;
     const { image, class: className, level } = req.body; // we use alias to avoid conflict with reserved keyword 'class'
 
@@ -164,26 +34,6 @@ export const updateSheet = async (req, res) => {
 };
 
 export const deleteSheet = async (req, res) => {
-    /**
-     * @openapi
-     * /sheet/{id}:
-     *   delete:
-     *     summary: Delete a sheet
-     *     tags: [Sheet]
-     *     description: Deletes a sheet by its unique ID.
-     *     parameters:
-     *       - in: path
-     *         name: id
-     *         required: true
-     *         description: Unique identifier of the sheet to delete.
-     *         schema:
-     *           type: string
-     *     responses:
-     *       204:
-     *         description: No Content, sheet deleted successfully.
-     *       404:
-     *         description: Not Found, sheet not found.
-     */
     const id = req.params.id;
     await sheetDataMapper.deleteSheet(id);
 
