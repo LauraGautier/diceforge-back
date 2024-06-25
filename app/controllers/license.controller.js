b69d6f70a5f57f14393d1b1007ef052e4956b5a2
Laura Gautier <lc.gautier@icloud.com>
2024-06-25 17:47:38 +0200

    Updated license

--- app/controllers/license.controller.js
+++ app/controllers/license.controller.js
@@ -4,6 +4,6 @@ import LicenseDataMapper from '../datamappers/license.datamapper.js';
 const licenseDataMapper = new LicenseDataMapper(pool);
 
 export const getAllLicenses = async (req, res) => {
-    const licenses = await licenseDataMapper.getAllLicenses();
-    res.status(200).json(licenses);
+    // const licenses = await licenseDataMapper.getAllLicenses();
+    res.status(200).json("licenses");
 };
